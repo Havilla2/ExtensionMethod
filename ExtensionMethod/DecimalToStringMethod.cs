27cbc63d05bcbc2d4315806ad4cb265a8c88c7d1
Ademola Balogun <havillaprinthub@gmail.com>
2022-11-09 01:16:13 -0800

    inital commit

---
+++ ExtensionMethod/DecimalToStringMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    public static class DecimalToStringMethod
+    {
+        public static string DecimalToString(this DecimalToSring decimalToSring, decimal num)
+        {
+            return num.ToString("C", CultureInfo.CurrentCulture);
+        }
+    }
+}
---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           DecimalToSring myDecimal = new DecimalToSring();
+            var result = myDecimal.DecimalToString(22.80m);
+           
+            Console.WriteLine($"{result}");
+
+           
+        }
+    }
+}
